67ca328588624967aa06bef862385e3bb513dba9
Bryan Guerra <bryan.guerra02@utrgv.edu>
2020-09-21 16:17:55 -0500

    updated comments

--- lib/utils/TimeConverter.dart
+++ lib/utils/TimeConverter.dart
@@ -1,5 +1,9 @@
 class TimeConverter {
   /// converts a seconds to its hours, minutes, seconds parts.
+  /// will return a list in the following format [hours, mins, seconds]
+  /// ex. convertSecondToHMS(30) -> [0,0,30]
+  /// ex. convertSecondToHMS(65) -> [0,1,5]
+  /// ex. convertSecondToHMS(3600) -> [1,0,0]
   static List<int> convertSecondToHMS(int seconds) {
     if (seconds < 0) {
       throw Exception('Seconds should not be negative');
